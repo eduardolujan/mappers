0c2fc291902c41cc1409c4ba88e71d054f86bdd9
Artem Malyshev <proofit404@gmail.com>
2020-06-25 18:58:34 +0300

    fixme: feat

--- src/_mappers/sources/sqlalchemy/factory.py
+++ src/_mappers/sources/sqlalchemy/factory.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
+from operator import methodcaller
+
+from _mappers.mapper import _Converter
+
+
 def _factory(fields, entity_factory, mapping):
-    pass
+    return _Query()
+
+
+class _Query(object):
+    converters = {
+        _Converter.entity: methodcaller("fetchone"),
+        _Converter.optional: lambda x: x,
+        _Converter.sequence: methodcaller("fetchall"),
+    }
+
+    def __call__(self, engine_result):
+        return engine_result
