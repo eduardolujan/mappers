1af10ea812b279db569a90c283fd850ea45d3e0e
Artem Malyshev <proofit404@gmail.com>
2020-06-25 18:58:50 +0300

    fixme: test

--- tests/helpers/sqlalchemy_project/fixtures.py
+++ tests/helpers/sqlalchemy_project/fixtures.py
@@ -4,4 +4,5 @@ from sqlalchemy_project.tables import metadata
 
 
 def setup():
+    """Load data."""
     metadata.create_all(engine)
--- tests/helpers/sqlalchemy_project/repositories.py
+++ tests/helpers/sqlalchemy_project/repositories.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+from functools import partial
+
 from sqlalchemy.sql import select
 
+from sqlalchemy_project.tables import DeliveryTable
 from sqlalchemy_project.tables import engine
+from sqlalchemy_project.tables import GroupTable
+from sqlalchemy_project.tables import MessageTable
 from sqlalchemy_project.tables import UserTable
 
 
@@ -16,8 +21,87 @@ def _get_load_users(mapper):
             self.connection = engine.connect()
 
         @mapper.reader.sequence
-        def load():
+        def load(self):
             query = select([UserTable])
             return self.connection.execute(query)
 
-    return Users().load
+    return partial(Users.load, Users())
+
+
+def _get_load_user(mapper):
+    class User(object):
+        def __init__(self):
+            self.connection = engine.connect()
+
+        @mapper.reader.entity
+        def load(self, primary_key):
+            query = select([UserTable]).where(UserTable.c.id == primary_key)
+            return self.connection.execute(query)
+
+    return partial(User.load, User())
+
+
+def _get_load_user_or_none(mapper):
+    class User(object):
+        def __init__(self):
+            self.connection = engine.connect()
+
+        @mapper.reader.optional
+        def load(self, primary_key):
+            query = select([UserTable]).where(UserTable.c.id == primary_key)
+            return self.connection.execute(query)
+
+    return partial(User.load, User())
+
+
+def _get_load_messages(mapper):
+    class Messages(object):
+        def __init__(self):
+            self.connection = engine.connect()
+
+        @mapper.reader.sequence
+        def load(self):
+            query = select([MessageTable])
+            return self.connection.execute(query)
+
+    return partial(Messages.load, Messages())
+
+
+def _get_load_total_messages(mapper, field_name):
+    class Messages(object):
+        def __init__(self):
+            self.connection = engine.connect()
+
+        @mapper.reader.sequence
+        def load(self):
+            # TODO: Count user_id as field_name.
+            query = select([MessageTable])
+            return self.connection.execute(query)
+
+    return partial(Messages.load, Messages())
+
+
+def _get_load_deliveries(mapper):
+    class Deliveries(object):
+        def __init__(self):
+            self.connection = engine.connect()
+
+        @mapper.reader.sequence
+        def load(self):
+            query = select([DeliveryTable])
+            return self.connection.execute(query)
+
+    return partial(Deliveries.load, Deliveries())
+
+
+def _get_load_groups(mapper):
+    class Groups(object):
+        def __init__(self):
+            self.connection = engine.connect()
+
+        @mapper.reader.sequence
+        def load(self):
+            query = select([GroupTable])
+            return self.connection.execute(query)
+
+    return partial(Groups.load, Groups())
